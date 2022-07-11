2f655ed8511b36db08133bff9bb4a21fe656314e
Vitalii Goncharov <1c.audit@gmail.com>
2022-07-11 20:40:56 +0300

    add editorconfig support

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -117,6 +117,9 @@ return packer.startup(function(use)
 
   -- DAP
 
+  -- Editorconfig support
+  use 'gpanders/editorconfig.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

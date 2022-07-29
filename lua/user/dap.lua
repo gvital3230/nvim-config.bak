66afbc8cb4ed4ef7621cd05fffde708acc801043
Vitalii Goncharov <1c.audit@gmail.com>
2022-07-29 09:26:58 +0300

    add lua dap + php remote

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -4,7 +4,7 @@ require("nvim-dap-virtual-text").setup()
 
 local dap = require('dap')
 
--- php local settings
+-- php
 dap.adapters.php = {
   type = 'executable',
   command = 'node',
@@ -16,6 +16,43 @@ dap.configurations.php = {
     type = 'php',
     request = 'launch',
     name = 'Listen for Xdebug',
-    port = 9003
+    port = 9003,
+  },
+  {
+    type = 'php',
+    request = 'launch',
+    name = 'Listen for Xdebug (with path mapping)',
+    port = 9003,
+    pathMappings = {
+        ['/var/www/html'] = "${workspaceFolder}",
+    },
   }
 }
+
+
+-- lua
+dap.adapters.nlua = function(callback, config)
+  callback({ type = 'server', host = config.host, port = config.port })
+end
+
+dap.configurations.lua = {
+  {
+    type = 'nlua',
+    request = 'attach',
+    name = "Attach to running Neovim instance",
+    host = function()
+      local value = vim.fn.input('Host [127.0.0.1]: ')
+      if value ~= "" then
+        return value
+      end
+      return '127.0.0.1'
+    end,
+    port = function()
+      local val = tonumber(vim.fn.input('Port: '))
+      assert(val, "Please provide a port number")
+      return val
+    end,
+  }
+}
+
+

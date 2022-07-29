293ed2f343c5254a371c18b4013edb159e3c85ee
Vitalii Goncharov <1c.audit@gmail.com>
2022-07-29 09:28:00 +0300

    add lua lsp path for installed plugins

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -1,16 +1,17 @@
 return {
-	settings = {
+  settings = {
 
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        -- library = {
+        --   [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        --   [vim.fn.stdpath("config") .. "/lua"] = true,
+        -- },
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    },
+  },
 }
